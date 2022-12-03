1e84b0e9f42ad4bf80b76d6252001b26752973e9
WASIF-M <wasif5690@gmail.com>
2022-12-03 14:10:41 +0500

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -15,22 +15,121 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 60,
+          title: Text(
+            'New Records',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+          backgroundColor: Color.fromRGBO(0, 103, 254, 50),
+          centerTitle: true,
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(50),
+              bottomRight: Radius.circular(50),
+            )),
+          ),
+        ),
         backgroundColor: Colors.white,
-        body: NewRecord(),
+        body: Form(),
       ),
     );
   }
 }
-class NewRecord extends StatefulWidget {
-  const NewRecord({Key? key}) : super(key: key);
+
+class Form extends StatefulWidget {
+  const Form({Key? key}) : super(key: key);
 
   @override
-  State<NewRecord> createState() => _NewRecordState();
+  State<Form> createState() => _FormState();
 }
 
-class _NewRecordState extends State<NewRecord> {
+class _FormState extends State<Form> {
+  void _showDatePicker(){
+    showDatePicker(context: context, initialDate: DateTime.now(), firstDate: DateTime(1966), lastDate: DateTime(2024));
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: "Project Name",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  )),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: "Supervisor",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  )),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: "Department",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  )),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                  hintText: "Group",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  )),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              children: [
+                MaterialButton(
+                  color: Color.fromRGBO(0, 103, 254, 50),
+                  onPressed:  _showDatePicker,
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Text(
+                      "Year",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: MaterialButton(
+                    color: Color.fromRGBO(0, 103, 254, 50),
+                    onPressed: () {},
+                    child: Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Text(
+                        "Grade",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
