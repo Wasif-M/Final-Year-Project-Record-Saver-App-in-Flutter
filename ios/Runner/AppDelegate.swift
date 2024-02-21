64de320db3760b0dae1e6c7a892901ff8b713fe7
Wasif Mehmood <116301395+Wasif-M@users.noreply.github.com>
2024-02-22 01:57:15 +0500

    Update AppDelegate.swift

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,7 +3,7 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
+  override func pplication(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
